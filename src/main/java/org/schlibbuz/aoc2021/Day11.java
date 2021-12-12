21e03ec95ffb9112b80b363305a22ccb3b19a795
Stefan Frei <stefan.a.frei@gmail.com>
2021-12-12 15:08:07 +0100

    boiler day11

---
+++ src/main/java/org/schlibbuz/aoc2021/Day11.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.schlibbuz.aoc2021;
+
+/**
+ *
+ * @author Stefan Frei <stefan.a.frei@gmail.com>
+ */
+public class Day11 extends Day {
+
+  Day11() {
+    this(RUN_TYPE.PROD);
+  }
+
+  Day11(RUN_TYPE runType) {
+    super(runType);
+  }
+
+  @Override
+  public long part1() {
+    return 0;
+  }
+
+  @Override
+  public long part2() {
+    return 0;
+  }
+
+}
