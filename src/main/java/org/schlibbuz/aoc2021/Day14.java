f22fb72b2ba254e1c39d6e14666995808def6e79
Stefan Frei <stefan.a.frei@gmail.com>
2021-12-15 16:52:26 +0100

    boiler

---
+++ src/main/java/org/schlibbuz/aoc2021/Day14.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.schlibbuz.aoc2021;
+
+/**
+ *
+ * @author Stefan Frei <stefan.a.frei@gmail.com>
+ */
+public class Day14 extends Day {
+
+  Day14() {
+    this(RUN_TYPE.PROD);
+  }
+
+  Day14(RUN_TYPE runType) {
+    super(runType);
+  }
+
+  @Override
+  public long part1() {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public long part2() {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+}
