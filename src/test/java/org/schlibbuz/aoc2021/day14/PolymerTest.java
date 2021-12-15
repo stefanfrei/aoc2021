571b3eb01fa374c8ebcaf49b254793358957215c
Stefan Frei <stefan.a.frei@gmail.com>
2021-12-15 18:35:24 +0100

    wip

---
+++ src/test/java/org/schlibbuz/aoc2021/day14/PolymerTest.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.schlibbuz.aoc2021.day14;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author stefanfrei
+ */
+public class PolymerTest {
+  @DataProvider(name = "growFix")
+  Object[][] growFix() {
+    return new Object[][] {
+      {
+        
+      },
+    };
+  }
+  @Test(dataProvider = "growFix")
+  public void growTest(List<String> data, String expected) {
+    
+  }
+}
